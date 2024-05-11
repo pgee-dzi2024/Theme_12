4ec42f7f05161a544d1c8085e8ff547f062d7911
Георги Бориков <pgee_dzi2024@abv.bg>
2024-05-11 13:15:27 +0300

    писмени работи

--- MyProject2/main/forms.py
+++ MyProject2/main/forms.py
@@ -3,5 +3,5 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='Потребителско име: ', widget=forms.TextInput(attrs={'class':'form-control'}))
-    password = forms.CharField(label='Парола: ', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    username = forms.CharField(label='Потребителско име: ', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Парола: ', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
